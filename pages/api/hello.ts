0dfd169bc512e0c594e4b80233095e681b1b1970
leeKB <tmwlak@gmail.com>
2022-08-03 20:43:20 +0900

    1st commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,99 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {pipe, flow} from 'fp-ts/function'
+import * as t from 'io-ts'
+import { fold } from 'fp-ts/Either'
 
-type Data = {
-  name: string
+export const dtoType = t.type({
+  name : t.string  
+})
+
+// type Data = {
+//   name: string
+// } | {}
+
+type ResType = {
+  isSuccess : boolean
+  msg : {
+    name: string
+  } | string    
 }
+
+// type dto = null 
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<ResType>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+
+  const doFailRes = (query : any) => {
+    res.json({
+      isSuccess : false
+      ,msg : JSON.stringify('method not allowed')
+    })
+  }
+
+  const doSuccessRes = (name : string ) => (randomVal : number) => {     
+    (randomVal < 0.5 )
+                      ? res.json({ 
+                            isSuccess : true
+                            ,msg : {
+                              name: `Hello ${name}`
+                            }
+                        })
+                      : res.json({
+                          isSuccess : false
+                          , msg : 'err occured in pipeling'
+                      })
+  }
+
+  pipe(
+    req.query
+    , dtoType.decode
+    , fold(
+        doFailRes
+        , (query) => {
+              // flow(Math.random
+              //   , doSuccessRes(query.name))
+              pipe(null
+                  , Math.random
+                  , doSuccessRes(query.name)
+                  )
+        }
+    )
+  )        
+
+  pipe(req.method
+    , (method) => method === 'GET'
+    )
+
+  if (req.method === 'GET')  {
+
+      console.log({query : req.query})
+
+      pipe(
+        req.query
+        , dtoType.decode
+        , fold(
+            doFailRes
+            , (query) => {
+                  // flow(Math.random
+                  //   , doSuccessRes(query.name))
+                  pipe(null
+                      , Math.random
+                      , doSuccessRes(query.name)
+                      )
+            }
+        )
+      )        
+  } else {
+    // res.status(400).end()
+    res.json({
+      isSuccess : false
+      ,msg : JSON.stringify('method not allowed')
+    })
+  }
+
+  // Math.random()
+  // res.status(200).json({ name: 'John Doe' })
 }
